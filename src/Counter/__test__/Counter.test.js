edf4199a2b3676e92fed873765d6c1d176f649a3
Karsenh <KarsenHansen@gmail.com>
2021-08-07 23:14:55 -0700

    First basic test added to test heading

---
+++ src/Counter/__test__/Counter.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Counter from '../Counter';
+import { render } from '@testing-library/react';
+// Allow use of 'expect' key-word
+import '@testing-library/jest-dom/extend-expect';
+
+// Takes two params - 1. Description of what it's doing 2. A function which will contain the logic for our test
+test('Header renders with correct text', () => {
+  // Render component in virtual dom
+  const component = render(<Counter />);
+
+  // Check if that component contains the heading text
+  const headerElement = component.getByTestId('header');
+
+  //
+  expect(headerElement.textContent).toBe('My Counter');
+});
